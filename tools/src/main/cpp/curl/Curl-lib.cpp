707ba4b8fe167704ffca4546261f6c05eb5420fb
Jun <375105540@qq.com>
2021-11-29 17:55:13 +0800

    update use Prefab publish prefab :soundTouch tools use prefab:openssl and curl and jsoncpp

---
+++ tools/src/main/cpp/curl/Curl-lib.cpp
@@ -0,0 +1,32 @@
+//
+// Created by stj on 2021/11/19.
+//
+#include <stdlib.h>
+#include <string.h>
+#include <jni.h>
+#include <android/log.h>
+
+
+extern "C"
+{
+
+#include "curl/curl.h"
+
+JNIEXPORT jstring JNICALL
+Java_me_shetj_sdk_curl_CUrlKit_downloadUrl(JNIEnv *env, jobject obj, jstring url) {
+    curl_version_info_data *ver = curl_version_info(CURLVERSION_NOW);
+//    CURL *curl = curl_easy_init();
+//    if(curl) {
+//        CURLcode res;
+//        curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
+//        res = curl_easy_perform(curl);
+//        curl_easy_cleanup(curl);
+//    }
+    __android_log_print((int)ANDROID_LOG_INFO, "curl", "libcurl version %d.%d.%d\n",
+           (ver->version_num >> 16) & 0xff,
+           (ver->version_num >> 8) & 0xff,
+           ver->version_num & 0xff);
+
+    return env->NewStringUTF("libcurl version");
+}
+}
---
+++ tools/src/main/java/me/shetj/sdk/curl/CUrlKit.kt
@@ -0,0 +1,19 @@
+package me.shetj.sdk.curl
+
+/**
+ *
+ * <b>@author：</b> shetj<br>
+ * <b>@createTime：</b> 2021/11/19<br>
+ * <b>@email：</b> 375105540@qq.com<br>
+ * <b>@describe</b>  <br>
+ */
+class CUrlKit {
+
+    companion object{
+        init {
+            System.loadLibrary("toolsCurl")
+        }
+    }
+
+    external fun downloadUrl(url: String?): String
+}
